ebd28be504bea5d1242c93b0fd7324187f288bc7
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2020-05-22 13:07:52 +0000

    More PEP8 fixes changed model to use Sequential

--- assignment2/cnn_cats_dogs.py
+++ assignment2/cnn_cats_dogs.py
@@ -40,72 +40,59 @@ val_batches = BatchGenerator(val_data, BATCH_SIZE, True, op)
 class CatsDogsModel(nn.Module):
     def __init__(self):
         super(CatsDogsModel, self).__init__()
-        # we start with a 7x7 kernel, padding 3, stride=2
-        # creating 64 feature maps
-        # (m, 3, 32, 32) -> (m, 64, 16, 16)
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, padding=3, stride=2)
-        # ReLU as activation
-        self.relu1 = nn.ReLU(inplace=True)
-        # and MaxPooling for dimension reduction
-        # (m, 64, 16, 16) -> (m, 64, 8, 8)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        # now comes a "block" - blowing up the features
-        # Step 1: Conv2D
-        # (m, 64, 8, 8) -> (m, 128, 8, 8)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        # another ReLU
-        self.relu2 = nn.ReLU(inplace=True)
-        # Step 2: another Conv2D
-        # (m, 128, 8, 8) -> (m, 128, 8, 8)
-        self.conv3 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        # another ReLU
-        self.relu3 = nn.ReLU(inplace=True)
-        # Step 3: MaxPooling for dimension reduction
-        # (m, 128, 8, 8) -> (m, 128, 4, 4)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        # now comes another "block" - blowing up the features
-        # Step 1: Conv2D
-        # (m, 128, 4, 4) -> (m, 256, 4, 4)
-        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        # another ReLU
-        self.relu4 = nn.ReLU(inplace=True)
-        # Step 2: another Conv2D
-        # (m, 256, 4, 4) -> (m, 256, 4, 4)
-        self.conv5 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        # another ReLU
-        self.relu5 = nn.ReLU(inplace=True)
-        # Step 3: MaxPooling for dimension reduction
-        # (m, 256, 4, 4) -> (m, 256, 2, 2)
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        # Average pool
-        # (m, 256, 2, 2) -> (m, 256, 1, 1)
-        self.avgpool = nn.AvgPool2d(kernel_size=2)
-        # Flatten
-        # (m, 256, 1, 1) -> (m, 256)
-        self.flatten = nn.Flatten()
-        # Linear layer for 2 classes
-        # (m, 256) -> (m, 2)
-        self.linear = nn.Linear(256, 2)
+        # Our network (one layer after the other in a list)
+        self.layers = nn.Sequential(
+            # we start with a 7x7 kernel, padding 3, stride=2
+            # creating 64 feature maps
+            # (m, 3, 32, 32) -> (m, 64, 16, 16)
+            nn.Conv2d(3, 64, kernel_size=7, padding=3, stride=2),
+            # ReLU as activation
+            nn.ReLU(inplace=True),
+            # and MaxPooling for dimension reduction
+            # (m, 64, 16, 16) -> (m, 64, 8, 8)
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # now comes a "block" - blowing up the features
+            # Step 1: Conv2D
+            # (m, 64, 8, 8) -> (m, 128, 8, 8)
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            # another ReLU
+            nn.ReLU(inplace=True),
+            # Step 2: another Conv2D
+            # (m, 128, 8, 8) -> (m, 128, 8, 8)
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            # another ReLU
+            nn.ReLU(inplace=True),
+            # Step 3: MaxPooling for dimension reduction
+            # (m, 128, 8, 8) -> (m, 128, 4, 4)
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # now comes another "block" - blowing up the features
+            # Step 1: Conv2D
+            # (m, 128, 4, 4) -> (m, 256, 4, 4)
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            # another ReLU
+            nn.ReLU(inplace=True),
+            # Step 2: another Conv2D
+            # (m, 256, 4, 4) -> (m, 256, 4, 4)
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            # another ReLU
+            nn.ReLU(inplace=True),
+            # Step 3: MaxPooling for dimension reduction
+            # (m, 256, 4, 4) -> (m, 256, 2, 2)
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # Average pool
+            # (m, 256, 2, 2) -> (m, 256, 1, 1)
+            nn.AvgPool2d(kernel_size=2),
+            # Flatten
+            # (m, 256, 1, 1) -> (m, 256)
+            nn.Flatten(),
+            # Linear layer for 2 classes
+            # (m, 256) -> (m, 2)
+            nn.Linear(256, 2)
+        )
 
     def forward(self, x):
         # run the steps ...
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.conv3(x)
-        x = self.relu3(x)
-        x = self.pool2(x)
-        x = self.conv4(x)
-        x = self.relu4(x)
-        x = self.conv5(x)
-        x = self.relu5(x)
-        x = self.pool3(x)
-        x = self.avgpool(x)
-        x = self.flatten(x)
-        x = self.linear(x)
-        return x
+        return self.layers.forward(x)
 
 
 def train_model(lr: float, wd: float) -> TrainedModel:
@@ -117,7 +104,9 @@ def train_model(lr: float, wd: float) -> TrainedModel:
 
     # Step 3: train CNN classifier, 100 epochs
     net = CatsDogsModel()
-    net.cuda()
+    # check whether GPU support is available
+    if torch.cuda.is_available():
+        net.cuda()
     clf = CnnClassifier(net, (0, 3, 32, 32), train_data.num_classes(), lr, wd)
 
     n_epochs = 100
@@ -136,7 +125,7 @@ def train_model(lr: float, wd: float) -> TrainedModel:
 
 
 # Step 4: random search for good parameter values
-best_model = TrainedModel(None, Accuracy()) # accuracy 0
+best_model = TrainedModel(None, Accuracy())  # accuracy 0
 best_lr = -1
 best_wd = -1
 NUM_ATTEMPTS = 1000
